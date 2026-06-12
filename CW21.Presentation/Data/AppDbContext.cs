0238aafdbe4444aaad818e47f9cf7b7925b2462d
Roham Roohnavaz <roohnavazroham12@gmail.com>
2026-06-12 16:03:46 +0330

    Add New MiddleWares & Action Result

--- CW21.Presentation/Data/AppDbContext.cs
+++ CW21.Presentation/Data/AppDbContext.cs
@@ -7,6 +7,14 @@ namespace CW21.Presentation.Data;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext()
+    {
+        
+    }
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    : base(options)
+    {
+    }
     public DbSet<Book> Books  { get; set; }
     public DbSet<Author>Authors { get; set; }
     public DbSet<Category> Categories {get; set;}
---
+++ WebApiPresention/Filters/ClientDetail.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApiPresention.Filters
+{
+    public class ClientDetail : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var ip = context.HttpContext.Connection.RemoteIpAddress?.ToString();
+            var userAgent = context.HttpContext.Request.Headers.UserAgent.ToString();
+
+            Console.WriteLine(ip);
+            Console.WriteLine(userAgent);
+        }
+    }
+}
